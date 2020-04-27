a552d73ce6b431c32650e9f64805fc28abdd1866
Douglas <Douglas@Douglass-MacBook-Pro.local>
2020-04-27 12:32:11 -0300

    Minimum deliverable version. First, simple rules added. 6 image added. Needs performance and perhaps speed optimization

--- find_flag.py
+++ find_flag.py
@@ -1,6 +1,7 @@
 import pyautogui as gui
 from PIL import Image
 import random
+import time
 
 class Minefield:
     def __init__(self, width, height, tiles):
@@ -14,7 +15,6 @@ class Tile:
         self.position = position
         self.value = value
 
-
 all_tiles = {
         '_': Image.open('Images/unseen.png'),
         0: Image.open('Images/none.png'),
@@ -23,6 +23,7 @@ all_tiles = {
         3: Image.open('Images/3.png'),
         4: Image.open('Images/4.png'),
         5: Image.open('Images/5.png'),
+        6: Image.open('Images/6.png'),
         'b': Image.open('Images/bomb.png'),
         'f': Image.open('Images/flag.png')
         }
@@ -42,25 +43,43 @@ def read_minefield(size = (760, 260, 1000, 720)):
     max_h, min_h = max([x['position'][1] for x in field]), min([x['position'][1] for x in field])    
     dist_betsq = field[1]['position'][0] - field[0]['position'][0]                                  # Currently 32 pixels
     
-    width = int(((max_w - min_w) + dist_betsq) / (dist_betsq - 1))
-    height = int(((max_h - min_h) + dist_betsq) / (dist_betsq - 1))
+    width = (max_w - min_w + dist_betsq) // dist_betsq
+    height = (max_h - min_h + dist_betsq) // dist_betsq
     
     tiles = [Tile(i, t['position'], t['value']) for i,t in enumerate(field)]
     minefield = Minefield(width, height, tiles)
     new_size = (minefield.tiles[0].position[0] - 5, minefield.tiles[0].position[1] - 5, minefield.tiles[len(minefield.tiles) - 1].position[0] - minefield.tiles[0].position[0] + 40, minefield.tiles[len(minefield.tiles) - 1].position[1] - minefield.tiles[0].position[1] + 40)
     print(size, new_size)
+    print("Width (Max, Min): ", max_w, min_w)
+    print("Height (Max, Min): ", max_h, min_h)
+    print("Dist_Bet_Sq: ", dist_betsq)
     return minefield, new_size
 
-    
+  
 def display_minefield(minefield):
     for tile in minefield.tiles:
         print(tile.value, end=' ')
         if (tile.index+1) % minefield.width == 0:
             print()
- 
-            
+     
+
+def find_neighbors(tile, minefield):
+    i, w, h = tile.index, minefield.width, minefield.height
+    if (i // w == 0) and (i % w == 0):          return (i + 1, i + w, i + w + 1)        # 1
+    elif (i // w == 0) and (i % w == w - 1):    return (i - 1, i + w, i + w - 1)        # 2
+    elif (i // w == h - 1) and (i % w == 0):     return (i - w, i - w + 1, i + 1)        # 3
+    elif (i // w == h - 1) and (i % w == w - 1): return (i - w - 1, i - w, i - 1)        # 4
+    elif i // w == 0:                           return (i - 1, i + 1, i + w - 1, i + w, i + w + 1)   # 5
+    elif i % w == 0:                            return (i - w, i - w + 1, i + 1, i + w, i + w + 1)      # 8
+    elif i % w == w - 1:                        return (i - w - 1, i - w, i - 1, i + w - 1, i + w)      # 6
+    elif i // w == h - 1:                       return (i - w - 1, i - w, i - w + 1, i - 1, i + 1)      # 7
+    else:                                       return (i - w - 1, i - w, i - w + 1, i - 1, i + 1, i + w - 1, i + w, i + w + 1) # 9       
+    
+           
 def play(minefield, size):
-    field = []
+    field, hidden_index = [], []
+    total_hidden = 0
+    click_random = True
     for tile in all_tiles:
         position = gui.locateAllOnScreen(all_tiles[tile], region=size)
         for p in position:
@@ -68,11 +87,52 @@ def play(minefield, size):
     
     field = sorted(field, key=lambda x: (x['position'][1], x['position'][0])) 
     minefield.tiles = [Tile(i, t['position'], t['value']) for i,t in enumerate(field)]
-
-    click = (random.randrange(len(minefield.tiles)))  
-    click_x, click_y = minefield.tiles[click].position[0]/2, minefield.tiles[click].position[1]/2
-    gui.click(click_x + 5, click_y + 5)      
-
+    
+    
+    # APPLY RULES:
+    for tile in minefield.tiles:
+        # TERMINATION CONDITION: If a bomb is seen, bot LOSES!
+        if tile.value == 'b': return True
+    
+        if tile.value == '_':                                               # Count the number of hidden tiles
+            total_hidden += 1
+            hidden_index.append(tile.index)
+         
+        neighbors = find_neighbors(tile, minefield)                         # Map the index of all neighbors
+        
+        if tile.value != '_' and tile.value != 'f' and tile.value != 0:     # If it is a number, count the number of hidden tiles around
+            n_hidden, n_flag = 0, 0
+            hidden_around = []
+            for n in neighbors:
+                if minefield.tiles[n].value == '_':
+                    n_hidden += 1
+                    hidden_around.append(n)                                 # List with the indexes of hidden tiles around
+                elif minefield.tiles[n].value == 'f':
+                    n_flag += 1                                             # Count number of flags around
+        
+            if tile.value == n_hidden + n_flag:                             # Place flags
+                for h in hidden_around:
+                    rClick_x, rClick_y = minefield.tiles[h].position[0]/2, minefield.tiles[h].position[1]/2
+                    gui.rightClick(rClick_x + 5, rClick_y + 5)
+                    minefield.tiles[h].value = 'f'
+                    click_random = False
+                    
+            elif tile.value == n_flag:                                      # Click on safe squares
+                for h in hidden_around:                    
+                    click_x, click_y = minefield.tiles[h].position[0]/2, minefield.tiles[h].position[1]/2
+                    gui.click(click_x + 5, click_y + 5)
+                    click_random = False
+    
+    # TERMINATION CONDITION: If no hidden tiles are left, bot WINS!
+    if total_hidden == 0: return True
+                   
+    if click_random == True:                                                # If no rule applies, click on a random square
+        next_click = random.choice(hidden_index)
+        click_x, click_y = minefield.tiles[next_click].position[0]/2, minefield.tiles[next_click].position[1]/2
+        gui.click(click_x + 5, click_y + 5)
+                            
+    return False
+    
 
 def main():
     #minefield = read_minefield()
@@ -81,19 +141,20 @@ def main():
     print(minefield.width, "x", minefield.height)
     
     # First click is chosen at random
-    firstClick = (random.randrange(len(minefield.tiles)))  
+    firstClick = random.randrange(len(minefield.tiles))  
     firstClick_x, firstClick_y = minefield.tiles[firstClick].position[0]/2, minefield.tiles[firstClick].position[1]/2
     gui.doubleClick(firstClick_x+5, firstClick_y+5)
-
     end = False
 
-    play(minefield, size)
-
-    # while end == False:
-    # end = play(minefield, size)
+    while end == False:
+        end = play(minefield, size)
+        time.sleep(0.005)
 
 
 main()
+
+
+
 
 '''tile = Image.open('Images/unseen.png')
 tiles = list(gui.locateAllOnScreen(tile))             # Returns tupples with left, top, width and height. Put them into list
